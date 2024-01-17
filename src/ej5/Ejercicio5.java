43889c0392c961185c85b74eb9a5d48d36c3b226
th3-m0th <th3.m0th@disroot.org>
2024-01-17 10:29:59 +0100

    Added exercises 5 & 6

--- src/ej5/Ejercicio5.java
+++ src/ej5/Ejercicio5.java
@@ -1,7 +1,50 @@
 package ej5;
 
-public class Ejercicio5 {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Ejercicio5 {
+    public static int[] reverseSort(int[] array) {
+        // guardaremos una copia del array
+        int[] numeros = new int[array.length];
+        numeros = Arrays.copyOf(array, array.length);
+        // guardará temporalmente el valor de un índice para cambiarlo de posición
+        int temp = 0;
+        for(int i = 0; i < numeros.length; i++) {
+            for(int j = i+1; j < numeros.length; j++) {
+                if(numeros[i] < numeros[j]) {
+                    temp = numeros[i];
+                    numeros[i] = numeros[j];
+                    numeros[j] = temp;
+                }
+            }
+        }
+
+        return numeros;
+    }
+
+    public static void main(String[] args) {
+        // guardará la puntuación de los 8 jugadores
+        int[] puntJugadores = new int[8];
+        // guardará temporalmente la puntuación de cada jugador
+        int puntuacion = 0;
+        Scanner sc = new Scanner(System.in);
+
+        // pregunta al usuario 8 veces por un número entre 1000 y 2800
+        for(int i = 0; i < puntJugadores.length; i++) {
+            do{
+                try{
+                    System.out.printf("(%d) " + "Introduce la puntuación del jugador (1000 - 2800)\n", i + 1);
+                    puntuacion = sc.nextInt();
+                }catch(InputMismatchException e){
+                    System.err.println("Dato erroneo.");
+                    sc.nextLine();
+                }
+            }while(puntuacion < 1000 || puntuacion > 2800);
+            puntJugadores[i] = puntuacion;
+        }
+        // muestra por pantalla el array ordenado de mayor a menor
+        System.out.println(Arrays.toString(reverseSort(puntJugadores)));
     }
 }
---
+++ src/ej6/Ejercicio6.java
@@ -0,0 +1,28 @@
+package ej6;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        // guardará 1000 números aleatorios
+        int[] aleatorios = new int[1000];
+        // guardará el número a buscar
+        int aBuscar;
+        // guardará el número de ocurrencias en el array
+        int ocurrencias = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Generando números:");
+        // generación de números aleatorios
+        for(int i = 0; i < aleatorios.length; i++) {
+            aleatorios[i] = (int) Math.random() * 100;
+        }
+
+        // obtenemos del usuario el númeroº
+        System.out.println("Introduce un número:");
+        aBuscar = sc.nextInt();
+
+
+
+    }
+}
